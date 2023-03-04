885c0116411af80f594c34da181dbb4d6f39a236
homodeus7 <plum.bus@yahoo.com>
2023-03-04 22:46:17 +0300

    added swiper

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,37 @@
 function scrollToSection(selector) {
-    console.log(window.screen.width);
-    let yOffset
-    if (window.screen.width < 780) {
-        yOffset = -39
-    } else {
-        yOffset = -58.5
-    }
-    const element = document.querySelector(selector);
-    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  console.log(window.screen.width);
+  let yOffset;
+  if (window.screen.width < 780) {
+    yOffset = -39;
+  } else {
+    yOffset = -58.5;
+  }
+  const element = document.querySelector(selector);
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
-    window.scrollTo({ top: y, behavior: 'smooth' });
+  window.scrollTo({ top: y, behavior: "smooth" });
 }
-
 
 const file = document.querySelector('input[name="user-file"]');
-const fileName = document.querySelector('.file_name')
+const fileName = document.querySelector(".file_name");
 
 function clearForm() {
-    fileName.innerText = ''
-    file.value = ''
+  fileName.innerText = "";
+  file.value = "";
 }
 
-file.onchange = e => {
-    const [file] = e.target.files
-    fileName.innerText = file.name
-}
+file.onchange = (e) => {
+  const [file] = e.target.files;
+  fileName.innerText = file.name;
+};
 
 function hideMenu() {
-    document.getElementById("menu__toggle").checked = false
+  document.getElementById("menu__toggle").checked = false;
 }
 
-const element = document.getElementById('phone');
+const element = document.getElementById("phone");
 const maskOptions = {
-    mask: '+{7} (000) 000-00-00'
+  mask: "+{7} (000) 000-00-00",
 };
 const mask = IMask(element, maskOptions);
 
@@ -41,71 +40,59 @@ const mask = IMask(element, maskOptions);
 // }, 5100);
 
 function openPopup(id) {
-    document.body.style.overflow = 'hidden'
-    document.querySelector(`#${id}`).classList.remove('hidden')
+  document.body.style.overflow = "hidden";
+  document.querySelector(`#${id}`).classList.remove("hidden");
 }
 
-const servicesItems = document.querySelectorAll('.popup2_content .popup_card')
+const servicesItems = document.querySelectorAll(".popup2_content .popup_card");
 
-servicesItems.forEach(item => item.addEventListener('click', () => {
+servicesItems.forEach((item) =>
+  item.addEventListener("click", () => {
     closePopup(true);
-    scrollToSection('.section6')
-}))
-
+    scrollToSection(".section6");
+  })
+);
 
 function closePopup(force) {
-    if (event.target.classList[0] !== 'popup_body' && event.target.tagName.toLowerCase() !== 'button' && !force) {
-        return
-    }
-    document.body.style.overflow = ''
-    document.querySelectorAll('.popup').forEach(elem => elem.classList.add('hidden'))
+  if (
+    event.target.classList[0] !== "popup_body" &&
+    event.target.tagName.toLowerCase() !== "button" &&
+    !force
+  ) {
+    return;
+  }
+  document.body.style.overflow = "";
+  document
+    .querySelectorAll(".popup")
+    .forEach((elem) => elem.classList.add("hidden"));
 }
+// import Swiper, { Pagination, Navigation } from "swiper";
+// Swiper.use([Pagination, Navigation]);
+// const swiper = new Swiper(".swiper1", {
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   direction: "horizontal",
+//   mousewheel: {
+//     forceToAxis: true,
+//   },
+//   slidesPerView: 4.4,
+//   spaceBetween: 14,
+//   clickable: true,
 
-const swiper = new Swiper('.swiper1', {
-    direction: 'horizontal',
-    mousewheel: {
-        forceToAxis: true
-    },
-    slidesPerView: 4.4,
-    spaceBetween: 14,
-    clickable: true,
-    breakpoints: {
-        300: {
-            slidesPerView: 1.2,
-            spaceBetween: 10
-        },
-        480: {
-            slidesPerView: 2.2,
-            spaceBetween: 15
-        },
-        640: {
-            slidesPerView: 2.4,
-            spaceBetween: 10
-        },
-        800: {
-            slidesPerView: 3.3,
-            spaceBetween: 20
-        },
-        1200: {
-            slidesPerView: 4.2,
-            spaceBetween: 40
-        },
-        1400: {
-            slidesPerView: 4.5,
-            spaceBetween: 40
-        }
-    }
-});
+//   speed: 1200,
+//   loop: true,
+// });
 
-const swiper2 = new Swiper('.swiper2', {
-    direction: 'horizontal',
-    mousewheel: {
-        forceToAxis: true
-    },
-    slidesPerView: 1,
-    clickable: true,
-    pagination: {
-        el: '.swiper-pagination2',
-    },
-
+const swiper2 = new Swiper(".swiper2", {
+  direction: "horizontal",
+  mousewheel: {
+    forceToAxis: true,
+  },
+  slidesPerView: 1,
+  clickable: true,
+  pagination: {
+    el: ".swiper-pagination2",
+  },
 });
