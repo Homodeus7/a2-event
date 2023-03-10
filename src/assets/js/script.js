6fd7da4896f26d18aabc3f2f67cdbb4a84fd3722
homodeus7 <plum.bus@yahoo.com>
2023-03-10 20:26:24 +0300

    added validation mask to phone

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,4 @@
 function scrollToSection(selector) {
-  console.log(window.screen.width);
   let yOffset;
   if (window.screen.width < 780) {
     yOffset = -30;
@@ -12,27 +11,6 @@ function scrollToSection(selector) {
   window.scrollTo({ top: y, behavior: "smooth" });
 }
 
-const fileName = document.querySelector(".file_name");
-const inputFile = document.querySelector('input[type="file"]');
-const textarea = document.querySelector("textarea");
-const inputText = document.querySelector('input[type="text"]');
-const inputEmail = document.querySelector('input[type="email"]');
-const inputTel = document.querySelector('input[type="tel"]');
-
-function clearForm() {
-  fileName.innerText = "";
-  inputFile.value = "";
-  textarea.value = "";
-  inputText.value = "";
-  inputText.value = "";
-  inputEmail.value = "";
-  inputTel.value = "";
-}
-
-function hideMenu() {
-  document.getElementById("menu__toggle").checked = false;
-}
-
 // setTimeout(() => {
 //     document.getElementById('loader').remove()
 // }, 5100);
@@ -43,13 +21,23 @@ function openPopup(id) {
 }
 
 const servicesItems = document.querySelectorAll(".popup2_content .popup_card");
-
 servicesItems.forEach((item) =>
   item.addEventListener("click", () => {
     closePopup(true);
     scrollToSection(".section6");
   })
 );
+
+const menuMobileItems = document.querySelectorAll(".menu__item");
+menuMobileItems.forEach((item) =>
+  item.addEventListener("click", () => {
+    hideMenu();
+  })
+);
+
+function hideMenu() {
+  document.getElementById("menu__toggle").checked = false;
+}
 
 function closePopup(force) {
   if (
@@ -68,15 +56,62 @@ function closePopup(force) {
 const clickExit = (evt) => {
   closePopup(evt.target.closest(".popup"));
 };
-const exit = Array.from(document.querySelectorAll(".popup__exit")).forEach(
-  (element) => {
-    element.addEventListener("click", clickExit);
-  }
-);
+const exit = Array.from(
+  document.querySelectorAll(".popup__exit .popup_btn")
+).forEach((element) => {
+  element.addEventListener("click", clickExit);
+});
+
 const closeCardsPopup = Array.from(
   document.querySelectorAll(".popup_card")
 ).forEach((element) => {
   element.addEventListener("click", clickExit);
+});
+
+//validation to form
+const input = document.body.querySelector(".phone");
+
+input.addEventListener("keypress", (evt) => {
+  if (evt.keyCode < 47 || evt.keyCode > 57) {
+    evt.preventDefault();
+  }
+});
+input.addEventListener("focus", () => {
+  if (input.value.length === 0) {
+    input.value = "+7";
+    input.selectionStart = input.value.length;
+  }
+});
+let old = 0;
+input.onkeydown = function () {
+  let curLen = input.value.length;
+  if (curLen < old) {
+    old--;
+    return;
+  }
+  if (curLen == 2) input.value = input.value + "(";
+  if (curLen == 6) input.value = input.value + ")-";
+  if (curLen == 11) input.value = input.value + "-";
+  if (curLen == 14) input.value = input.value + "-";
+  old++;
+};
+input.addEventListener("click", (evt) => {
+  if (input.selectionStart < 2) {
+    input.selectionStart = input.value.length;
+  }
+  if (evt.key === "Backspace" && input.value.length <= 2) {
+    evt.preventDefault();
+  }
+});
+input.addEventListener("blur", () => {
+  if (input.value === "+7") {
+    input.value = "";
+  }
+});
+input.addEventListener("keydown", (evt) => {
+  if (evt.key === "Backspace" && input.value.length <= 2) {
+    evt.preventDefault();
+  }
 });
 
 let inputs = document.querySelectorAll(".input__file");
@@ -93,3 +128,40 @@ Array.prototype.forEach.call(inputs, function (input) {
     else label.querySelector(".input-file-btn").innerText = labelVal;
   });
 });
+
+const fileName = document.querySelector(".file_name");
+const inputFile = document.querySelector('input[type="file"]');
+const textarea = document.querySelector("textarea");
+const inputText = document.querySelector('input[type="text"]');
+const inputEmail = document.querySelector('input[type="email"]');
+const inputTel = document.querySelector('input[type="tel"]');
+
+function clearForm() {
+  fileName.innerText = "";
+  inputFile.value = "";
+  textarea.value = "";
+  inputText.value = "";
+  inputText.value = "";
+  inputEmail.value = "";
+  inputTel.value = "";
+}
+
+// let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+// let popup = document.querySelector('.popup'); // Само окно
+// let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+// let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+// openPopupButtons.forEach((button) => { // Перебираем все кнопки
+//   button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+//       e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+//       popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+//       popup.classList.add('active'); // И для самого окна
+//   })
+// });
+
+// document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+//   if(e.target === popupBg) { // Если цель клика - фот, то:
+//       popupBg.classList.remove('active'); // Убираем активный класс с фона
+//       popup.classList.remove('active'); // И с окна
+//   }
+// });
